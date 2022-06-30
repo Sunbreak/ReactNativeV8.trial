2dee6bbce7a47c9fd4fc45792544dae8d1bf3e78
wangkun <wangkun@xiaohongshu.com>
2022-06-30 14:37:19 +0800

    Use react-native-v8
    Signed-off-by: wangkun <wangkun@xiaohongshu.com>

--- android/app/src/main/java/com/reactnativev8/MainApplication.java
+++ android/app/src/main/java/com/reactnativev8/MainApplication.java
@@ -2,16 +2,23 @@ package com.reactnativev8;
 
 import android.app.Application;
 import android.content.Context;
+
+import androidx.annotation.Nullable;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.config.ReactFeatureFlags;
+import com.facebook.react.modules.systeminfo.AndroidInfoHelpers;
 import com.facebook.soloader.SoLoader;
 import com.reactnativev8.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+import io.csie.kudo.reactnative.v8.executor.V8ExecutorFactory;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -34,6 +41,17 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Nullable
+        @Override
+        protected JavaScriptExecutorFactory getJavaScriptExecutorFactory() {
+          return new V8ExecutorFactory(
+            getApplicationContext(),
+            getPackageName(),
+            AndroidInfoHelpers.getFriendlyDeviceName(),
+            getUseDeveloperSupport()
+          );
         }
       };
 
